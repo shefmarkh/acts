672f565a76650e3619f43ae6817cdaef72b91bc7
Mark Hodgkinson <markhodgkinson@ITAsset02240Air.home>
2024-02-12 14:38:22 +0000

    Add PFlowSurfaces json reading test.

---
+++ Tests/UnitTests/Core/Detector/PFlowSurfaces.cpp
@@ -0,0 +1,19 @@
+#include <boost/test/unit_test.hpp>
+
+#include "Acts/Plugins/Json/ActsJson.hpp"
+
+//#include <nlohmann/json.hpp>
+
+//using namespace Acts::Experimental;
+
+BOOST_AUTO_TEST_SUITE(Detector)
+
+BOOST_AUTO_TEST_CASE(PFlowJsonSurfacesReader){
+
+    nlohmann::json j;
+
+
+    BOOST_TEST(true);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
