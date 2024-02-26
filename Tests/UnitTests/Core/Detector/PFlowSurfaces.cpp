4f5d737cea1c86ae9d874a1bf08e03061a5d6e7a
Mark Hodgkinson <markhodgkinson@ITAsset02240Air.home>
2024-02-26 14:22:40 +0000

    Add building of volumes.

--- Tests/UnitTests/Core/Detector/PFlowSurfaces.cpp
+++ Tests/UnitTests/Core/Detector/PFlowSurfaces.cpp
@@ -3,15 +3,18 @@
 #include <fstream>
 #include <iostream>
 
+#include "Acts/Detector/DetectorVolume.hpp"
+#include "Acts/Detector/PortalGenerators.hpp"
+#include "Acts/Geometry/CylinderVolumeBounds.hpp"
 #include "Acts/Geometry/GeometryHierarchyMap.hpp"
 #include "Acts/Geometry/GeometryIdentifier.hpp"
+#include "Acts/Navigation/SurfaceCandidatesUpdaters.hpp"
 #include "Acts/Plugins/Json/ActsJson.hpp"
 #include "Acts/Plugins/Json/GeometryHierarchyMapJsonConverter.hpp"
 #include "Acts/Plugins/Json/SurfaceJsonConverter.hpp"
 #include "Acts/Surfaces/Surface.hpp"
 
-//using namespace Acts::Experimental;
-
+using namespace Acts::Experimental;
 
 using GeometryIdHelper = Acts::GeometryHierarchyMapJsonConverter<bool>;
 using SurfaceContainer = Acts::GeometryHierarchyMap<std::shared_ptr<const Acts::Surface>>;
@@ -31,14 +34,20 @@ BOOST_AUTO_TEST_CASE(PFlowJsonSurfacesReader){
     auto jsonSurfaces = json["entries"];
 
     std::vector<SurfaceContainer::InputElement> surfaceElements;
+    std::vector<Acts::Transform3> surfaceTransforms;
 
     for (const auto& jsonSurface : jsonSurfaces) {
         Acts::GeometryIdentifier geoId = GeometryIdHelper::decodeIdentifier(jsonSurface);        
         auto surface = Acts::SurfaceJsonConverter::fromJson(jsonSurface["value"]);
         surfaceElements.emplace_back(geoId, surface);
+
+        nlohmann::json jTransform = jsonSurface["translation"];
+        Acts::Transform3 sTransform = Acts::Transform3JsonConverter::fromJson(jTransform);
+        surfaceTransforms.push_back(sTransform);
     }
 
     BOOST_CHECK_EQUAL(surfaceElements.size(), 1);
+    BOOST_CHECK_EQUAL(surfaceTransforms.size(),1);
 
     //Our test json file only has one surface in it
     Acts::GeometryIdentifier geoID = surfaceElements[0].first;
@@ -64,7 +73,47 @@ BOOST_AUTO_TEST_CASE(PFlowJsonSurfacesReader){
     BOOST_CHECK_CLOSE(testRadius,readRadius,floatingPointTolerance);
     BOOST_CHECK_CLOSE(testZ,readZ,floatingPointTolerance);
     BOOST_CHECK_CLOSE(testPhi,readPhi,floatingPointTolerance);
+
+    //Now lets build a volume
+
+    //First create boundaries for cylinder
+    //Parameters are inner radius, outer radius and half length
+    //Make them slightly larger than the cylinder we have
+    auto fullCylinderBounds =
+      std::make_unique<Acts::CylinderVolumeBounds>(0., readRadius+1.0, (readZ/2)+1.0);
+
+    Acts::Transform3 nominal = Acts::Transform3::Identity();
+    Acts::GeometryContext tContext;
+    auto portalGenerator = defaultPortalGenerator();
+
+    auto fullCylinderVolume = DetectorVolumeFactory::construct(
+      portalGenerator, tContext, "FullCylinderVolume", nominal,
+      std::move(fullCylinderBounds), tryAllPortals());
     
+    BOOST_CHECK(fullCylinderVolume->surfaces().empty());
+    BOOST_CHECK(fullCylinderVolume->volumes().empty());
+    BOOST_CHECK_EQUAL(fullCylinderVolume->portals().size(), 3u);
+
+    auto vector3 = fullCylinderVolume->center(tContext);
+    std::cout << "x,y,x are " << vector3.x() << ", " << vector3.y() << " and " << vector3.z() << std::endl;
+
+    //Now create a cylinder shifted in Z by our transform coordinates
+    Acts::Transform3 surfaceTransform = surfaceTransforms[0];
+    auto translation = surfaceTransform.translation();
+    std::cout << "Values are " << translation[0] << ", " << translation[1] << " and " <<
+    translation[2] << std::endl;
+
+    auto transformedCylinderBounds =
+      std::make_unique<Acts::CylinderVolumeBounds>(0., readRadius+1.0, (readZ/2)+1.0);
+
+    auto fullTransformedCylinderVolume = DetectorVolumeFactory::construct(
+      portalGenerator, tContext, "FullTransformedCylinderVolume", surfaceTransform,
+      std::move(transformedCylinderBounds), tryAllPortals());
+
+    auto vector3Transformed = fullTransformedCylinderVolume->center(tContext);
+    std::cout << "x,y,x are " << vector3Transformed.x() << ", " << 
+    vector3Transformed.y() << " and " << vector3Transformed.z() << std::endl;
+
 }
 
 BOOST_AUTO_TEST_SUITE_END()
