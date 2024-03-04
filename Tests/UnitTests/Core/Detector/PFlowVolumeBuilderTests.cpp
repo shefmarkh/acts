b719713b2a6ea44cff850173c3af79b2dc6114a7
Mark Hodgkinson <markhodgkinson@ITAsset02240Air.home>
2024-03-04 14:12:13 +0000

    Add first version of pflow volume builder tests.

---
+++ Tests/UnitTests/Core/Detector/PFlowVolumeBuilderTests.cpp
@@ -0,0 +1,31 @@
+#include <boost/test/unit_test.hpp>
+
+#include "Acts/Detector/interface/IExternalStructureBuilder.hpp"
+#include "Acts/Geometry/CylinderVolumeBounds.hpp"
+
+using namespace Acts;
+using namespace Acts::Experimental;
+
+template <typename bounds_type>
+class ExternalsBuilder : public IExternalStructureBuilder {
+ public:
+  ExternalsBuilder(const Transform3& transform, const bounds_type& bounds)
+      : IExternalStructureBuilder(),
+        m_transform(transform),
+        m_bounds(std::move(bounds)) {}
+
+  ExternalStructure construct(
+      [[maybe_unused]] const GeometryContext& gctx) const final {
+    return {m_transform, std::make_unique<bounds_type>(m_bounds),
+            defaultPortalGenerator()};
+  }
+
+ private:
+  Transform3 m_transform = Transform3::Identity();
+  bounds_type m_bounds;
+};
+BOOST_AUTO_TEST_CASE(pflowVolumers){
+  CylinderVolumeBounds cvBounds(10, 100, 200);
+  auto cBuilder = std::make_shared<ExternalsBuilder<CylinderVolumeBounds>>(
+      Transform3::Identity(), cvBounds);
+}
