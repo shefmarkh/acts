fcb5383dcf5eab963a34c6a59fad1f173849c17d
Mark Hodgkinson <markhodgkinson@ITAsset02240Air.home>
2024-03-28 10:00:13 +0000

    Adjust how the cylinder builders are used. Add code to build a full detector.

--- Tests/UnitTests/Core/Detector/PFlowATLASCaloBarrelCylinderBuilderTests.cpp
+++ Tests/UnitTests/Core/Detector/PFlowATLASCaloBarrelCylinderBuilderTests.cpp
@@ -1,6 +1,7 @@
 #include <boost/test/unit_test.hpp>
 
 #include "Acts/Detector/CylindricalContainerBuilder.hpp"
+#include "Acts/Detector/DetectorBuilder.hpp"
 #include "Acts/Detector/DetectorVolumeBuilder.hpp"
 #include "Acts/Detector/interface/IExternalStructureBuilder.hpp"
 #include "Acts/Detector/LayerStructureBuilder.hpp"
@@ -35,7 +36,7 @@ class ExternalsBuilder : public IExternalStructureBuilder {
 GeometryContext tContext;
 
 void addGapVolumeBuilder(const double& rMin, const double& rMax, const double& halfLengthZ, const std::string& gapName,
-std::vector<std::shared_ptr<DetectorVolumeBuilder> >& builders){
+std::vector<std::shared_ptr<const IDetectorComponentBuilder> >& builders){
 
   CylinderVolumeBounds gapBounds(rMin, rMax, halfLengthZ);
 
@@ -53,7 +54,7 @@ std::vector<std::shared_ptr<DetectorVolumeBuilder> >& builders){
 
 }
 
-void addVolumeBuilder(const std::string& jsonFileName, std::vector<std::shared_ptr<DetectorVolumeBuilder> >& builders, const double& layerRMin, const double& layerRMax, 
+void addVolumeBuilder(const std::string& jsonFileName, std::vector<std::shared_ptr<const IDetectorComponentBuilder> >& builders, const double& layerRMin, const double& layerRMax, 
 const double& layerHalfLengthZ, const std::string& cylinderName){
 
   CylinderVolumeBounds cylinderBounds(layerRMin, layerRMax, layerHalfLengthZ);
@@ -89,8 +90,7 @@ const double& layerHalfLengthZ, const std::string& cylinderName){
 BOOST_AUTO_TEST_CASE(ATLASCaloBarrelTests){
     
     CylindricalContainerBuilder::Config ccConfig;
-
-    std::vector<std::shared_ptr<DetectorVolumeBuilder> > builders = {};
+    ccConfig.builders = std::vector<std::shared_ptr<const IDetectorComponentBuilder> >();
 
     double caloRMin = 0.00;//looping over all DDE in preSamplerB in Athena shows the minimal value of r is 1452.46
     double calorRMax = 3632.00;//looping over all DDE in TileBar2, TileGap2 and Tile Ext2 in Athena shows the maximal value of r is 3630.00
@@ -99,28 +99,39 @@ BOOST_AUTO_TEST_CASE(ATLASCaloBarrelTests){
     double preSamplerBRMin = 1450.00;//looping over all DDE in preSamplerB in Athena shows the minimal value of r is 1452.46
 
     //create a cylinder inside preSamplerB
-    addGapVolumeBuilder(caloRMin, preSamplerBRMin, caloHalfLengthZ,"Gap0", builders);
+    addGapVolumeBuilder(caloRMin, preSamplerBRMin, caloHalfLengthZ,"Gap0", ccConfig.builders);
 
     //PreSamplerB
     double preSamplerBRMax = 1462.00;//looping over all DDE in preSamplerB in Athena shows the maximal value of r is 1460.87
     double preSamplerBHalfLengthZ = 3146;//Looping over all DDE in preSamplerB in Athena shows the half length between min and maz Z is 3144.46
-    addVolumeBuilder("CylinderSurfaces_PreSamplerB.json",builders,preSamplerBRMin, preSamplerBRMax, preSamplerBHalfLengthZ,"preSamplerB");
+    addVolumeBuilder("CylinderSurfaces_PreSamplerB.json",ccConfig.builders,preSamplerBRMin, preSamplerBRMax, preSamplerBHalfLengthZ,"preSamplerB");
 
     //gap between preSamplerB and EMB1. According to CaloDetDescrElements in Athena:
     //PreSamplerB: MinR and maxR in PreSamplerB are 1452.46 1460.87
     //EMB1: MinR and maxR in EMB1 are 1512.42 1547.71
     //EMB1: Half length in Z for EMB1 is 3097.51
-    addGapVolumeBuilder(1462,1510,caloHalfLengthZ, "Gap1",builders);
+    addGapVolumeBuilder(1462,1510,caloHalfLengthZ, "Gap1",ccConfig.builders);
 
     //EMB1 cylinder
-    addVolumeBuilder("CylinderSurfaces_EMB1.json",builders,1510,1549,3099,"EMB1");
+    addVolumeBuilder("CylinderSurfaces_EMB1.json",ccConfig.builders,1510,1549,3099,"EMB1");
 
     //Gap between EMB1 and EMB2.  According to CaloDetDescrElements in Athena:
     //EMB2: MinR and maxR in EMB2 are 1656.85 1762.55
     //EMB2: Half length in Z for EMB2 is 3439.76
-    addGapVolumeBuilder(1549,1654,caloHalfLengthZ,"Gap2",builders);
+    addGapVolumeBuilder(1549,1654,caloHalfLengthZ,"Gap2",ccConfig.builders);
 
     //EMB2 cylinder
-    addVolumeBuilder("CylinderSurfaces_EMB2.json",builders,1654,1764,3441,"EMB2");
+    addVolumeBuilder("CylinderSurfaces_EMB2.json",ccConfig.builders,1654,1764,3441,"EMB2");
+
+    auto barrelCylinders = std::make_shared<CylindricalContainerBuilder>(
+      ccConfig, getDefaultLogger("ATLASCaloCylindricalContainerBuilder", Logging::VERBOSE));
+
+    Acts::Experimental::DetectorBuilder::Config dCfg;
+    dCfg.auxiliary = "*** Test : auto generated ATLAS barrel calo builder  ***";
+    dCfg.name = "ATLASCaloBarrelCylinderBuilder";
+    dCfg.builder = barrelCylinders;
+
+    auto detector = Acts::Experimental::DetectorBuilder(dCfg).construct(tContext);
+
 
 }
