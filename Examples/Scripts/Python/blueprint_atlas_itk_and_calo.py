ba5a6c088b90e78ac5cf9a5b0256a6479bda89fa
Mark Hodgkinson <markhodgkinson@IT-Asset-0000002240-MacBook-Air.local>
2025-05-09 15:46:45 +0100

    Add EMBarrel Gap Volumes between layers

--- Examples/Scripts/Python/blueprint_atlas_itk_and_calo.py
+++ Examples/Scripts/Python/blueprint_atlas_itk_and_calo.py
@@ -180,14 +180,26 @@ def build_calo_EMBarrel(CaloDimensions, atlas: acts.BlueprintNode):
         PreSamplerB.addStaticVolume(
             base, acts.CylinderVolumeBounds(CaloDimensions["PreSamplerBMinR"], CaloDimensions["PreSamplerBMaxR"], CaloDimensions["PreSamplerBHalfLengthZ"]), name="PreSamplerB"
         )
+    with atlas.CylinderContainer("EMBarrelGap1", aDir.AxisZ) as EMBarrelGap1:
+        EMBarrelGap1.addStaticVolume(
+            base, acts.CylinderVolumeBounds(CaloDimensions["PreSamplerBMaxR"], CaloDimensions["EMB1MinR"], CaloDimensions["CaloHalfLengthZ"]), name="EMBarrelGap1"
+        )
     with atlas.CylinderContainer("EMB1", aDir.AxisZ) as EMB1:
         EMB1.addStaticVolume(
             base, acts.CylinderVolumeBounds(CaloDimensions["EMB1MinR"], CaloDimensions["EMB1MaxR"], CaloDimensions["EMB1HalfLengthZ"]), name="EMB1"
+        )
+    with atlas.CylinderContainer("EMBarrelGap2", aDir.AxisZ) as EMBarrelGap2:
+        EMBarrelGap2.addStaticVolume(
+            base, acts.CylinderVolumeBounds(CaloDimensions["EMB1MaxR"], CaloDimensions["EMB2MinR"], CaloDimensions["CaloHalfLengthZ"]), name="EMBarrelGap2"
         )
     with atlas.CylinderContainer("EMB2", aDir.AxisZ) as EMB2:
             EMB2.addStaticVolume(
                 base, acts.CylinderVolumeBounds(CaloDimensions["EMB2MinR"], CaloDimensions["EMB2MaxR"], CaloDimensions["EMB2HalfLengthZ"]), name="EMB2"
             )
+    with atlas.CylinderContainer("EMBarrelGap3", aDir.AxisZ) as EMBarrelGap3:
+        EMBarrelGap3.addStaticVolume(
+            base, acts.CylinderVolumeBounds(CaloDimensions["EMB2MaxR"], CaloDimensions["EMB3MinR"], CaloDimensions["CaloHalfLengthZ"]), name="EMBarrelGap3"
+        )
     with atlas.CylinderContainer("EMB3", aDir.AxisZ) as EMB3:
             EMB3.addStaticVolume(
                 base, acts.CylinderVolumeBounds(CaloDimensions["EMB3MinR"], CaloDimensions["EMB3MaxR"], CaloDimensions["EMB3HalfLengthZ"]), name="EMB3"
